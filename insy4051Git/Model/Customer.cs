e94f3befaa9b69279389b43f9f175befae23ba65
Zoomi22 <ben.zummo@marquette.edu>
2023-11-28 15:05:56 -0600

    Created the model folder and the customer class

---
+++ insy4051Git/Model/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace insy4051Git.NewFolder
+{
+    internal class Customer
+    {
+
+    }
+}
